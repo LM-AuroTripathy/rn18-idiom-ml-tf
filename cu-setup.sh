befbebf1ff1bf2ed13f7904cc03a7c3b37023fa9
Auro Tripathy <auro@lightmatter.co>
2022-09-06 22:41:29 -0700

    init and working

---
+++ cu-setup.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/mikhail/cuda/lib64:/usr/local/lib:/usr/local/cuda/lib:/usr/local/cuda/lib64:/usr/local/lib:/usr/local/cuda/lib:/usr/local/cuda/lib64
+export LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+export PYTHONPATH=. 
