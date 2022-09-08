ad0b27ceadd5536ce51bd92f13c06c01cb2dce74
Auro Tripathy <auro@lightmatter.co>
2022-09-08 15:29:32 -0700

    init

--- run_main.sh
+++ run_main.sh
@@ -5,8 +5,22 @@
 #        --eval-batch-size 16
 
 
+# python main.py \
+#        --data-dir /models/home/auro/pyt-rn18-notebook/imagewoof2-320 \
+#        --checkpoint-path /models/home/auro/bk-idiom-ml-tf/rn18/checkpoint/rn18-best-epoch-43-acc-0.7.hdf5 \
+#        --do-envise-eval \
+#        --eval-batch-size 16
+
+
+
 python main.py \
        --data-dir /models/home/auro/pyt-rn18-notebook/imagewoof2-320 \
        --checkpoint-path /models/home/auro/bk-idiom-ml-tf/rn18/checkpoint/rn18-best-epoch-43-acc-0.7.hdf5 \
-       --do-envise-eval \
-       --eval-batch-size 16
+       --do-tune \
+       --lr 1e-3 \
+       --epochs 1 \
+       --tune-batch-size 16
+
+
+
+
