2913b9790b4c61b95798e1ea2058ddf25c73fb41
Auro Tripathy <auro@lightmatter.co>
2022-09-08 14:03:23 -0700

    init

---
+++ run_main.sh
@@ -0,0 +1,5 @@
+python main.py \
+       --data-dir /models/home/auro/pyt-rn18-notebook/imagewoof2-320 \
+       --checkpoint-path /models/home/auro/bk-idiom-ml-tf/rn18/checkpoint/rn18-best-epoch-43-acc-0.7.hdf5 \
+       --do-oob-eval \
+       --eval-batch-size 16
