c79cb32edf3935b4a427fd3d35690afc7fdce8f5
Auro Tripathy <auro@lightmatter.co>
2022-09-12 14:34:31 -0700

    init

---
+++ run_onnx.sh
@@ -0,0 +1,26 @@
+# python convert2onnx.py \
+#        --data-dir /models/home/auro/pyt-rn18-notebook/imagewoof2-320 \
+#        --checkpoint-path /models/home/auro/bk-idiom-ml-tf/rn18/checkpoint/rn18-best-epoch-43-acc-0.7.hdf5 \
+#        --do-oob-eval \
+#        --eval-batch-size 16
+
+
+# python conver2onnx.py \
+#        --data-dir /models/home/auro/pyt-rn18-notebook/imagewoof2-320 \
+#        --checkpoint-path /models/home/auro/bk-idiom-ml-tf/rn18/checkpoint/rn18-best-epoch-43-acc-0.7.hdf5 \
+#        --do-envise-eval \
+#        --eval-batch-size 16
+
+
+
+python convert2onnx.py \
+       --data-dir /models/home/auro/pyt-rn18-notebook/imagewoof2-320 \
+       --checkpoint-path /models/home/auro/bk-idiom-ml-tf/rn18/checkpoint/rn18-best-epoch-43-acc-0.7.hdf5 \
+       --do-tune \
+       --lr 1e-5 \
+       --epochs 1 \
+       --tune-batch-size 16
+
+
+
+
