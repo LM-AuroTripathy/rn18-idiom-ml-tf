391f14eae26bd6c7b08f57a11f7b165de7f7a412
Auro Tripathy <auro@lightmatter.co>
2022-09-14 18:58:47 -0700

    args

--- tf-profile.py
+++ tf-profile.py
@@ -1,5 +1,6 @@
 import os
 import time
+import argparse
 
 import keras
 import tensorflow as tf
@@ -11,11 +12,25 @@ import itertools
 
 from idiom.runtime import profile
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Compile the ONNX model for Envise')
+    parser.add_argument('--data-dir',
+                        type=str, 
+                        help='Path to ResNet18 dataset', required=True)
+    parser.add_argument('--compiled-model-dir',
+                        type=str, 
+                        help='Path to compiled output', required=True)
+    args = parser.parse_args()
+
+    return args
+
+
 # data_dir = '../../00-getting-started/imagewoof2-320'
-data_dir = '/idiom-eap/examples/00-getting-started/imagewoof2-320'
-compiled_model_dir = './compile_dir'
-val_folder = os.path.join(data_dir, 'val')
-train_folder = os.path.join(data_dir, 'train')
+# data_dir = '/idiom-eap/examples/00-getting-started/imagewoof2-320'
+# compiled_model_dir = './compile_dir'
+args = parse_arguments()
+val_folder = os.path.join(args.data_dir, 'val')
+# train_folder = os.path.join(args.data_dir, 'train')
 target_size = (320, 320)
 channels = (3,)
 nb_classes = 10
@@ -26,16 +41,12 @@ image_gen = ImageDataGenerator(featurewise_center=True,
 image_gen.mean = np.array([123.68, 116.779, 103.939],
                           dtype=np.float32).reshape((1, 1, 3))  # ordering: [R, G, B]
 image_gen.std = 64.
-val_folder = os.path.join(data_dir, 'val')
+# val_folder = os.path.join(data_dir, 'val')
 val_gen = image_gen.flow_from_directory(val_folder, class_mode="categorical",
                                         shuffle=False, batch_size=eval_batch_size,
                                         target_size=target_size)
 x_batch, y_batch = next(val_gen)
 print(f'x shape: {x_batch.shape} y shape: {y_batch.shape}')
 data = [{'input': np.expand_dims(x, axis=0) for x in itertools.islice(x_batch, eval_batch_size)}]
-# data = [{'input': np.repeat(np.expand_dims(x, axis=0), 16, axis=0) for x in itertools.islice(x_batch, eval_batch_size)}]
 
-
-print(data)
-
-profile(compiled_model_dir, data, detailed_report=True)
+profile(args.compiled_model_dir, data, detailed_report=True)
